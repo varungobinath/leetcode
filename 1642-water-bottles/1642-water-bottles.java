08b36c72d3924b5f479e2348714c326d383f0e65
Varun Gobinath <137699472+varungobinath@users.noreply.github.com>
2025-02-09 12:41:40 +0530

    Time: 0 ms (100%), Space: 40.8 MB (11.56%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,12 +1,11 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int total_bottle = numBottles;
-        while(numBottles/numExchange!=0){
+        while(numBottles>=numExchange){
             int fullBottle = numBottles/numExchange;
             int empty = numBottles%numExchange;
             total_bottle += fullBottle;
             numBottles = fullBottle+empty;
-            // System.out.println(fullBottle+" "+empty);
         }
         return total_bottle;
     }
