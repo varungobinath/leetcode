c0e17ee07e2e1270bac1ae6d1fbc02ee1256f4ba
Varun Gobinath <137699472+varungobinath@users.noreply.github.com>
2025-05-02 13:13:04 +0530

    Time: 0 ms (100%), Space: 41.6 MB (66.31%) - LeetHub

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -5,14 +5,14 @@ class Solution {
         for(int i=n-1;i>=0;i--){
             char ch = s.charAt(i);
             if(ch=='#'){
-                int pos = (s.charAt(i-2)-'0')*10+(s.charAt(i-1)-'0');
-                ans.insert(0,(char)('a'+pos-1));
+                int pos = (s.charAt(i-2)-'0')*10+(s.charAt(i-1)-'0')-1;
+                ans.append((char)('a'+pos));
                 i-=2;
             }else{
                 char alp = (char)('a'+ch-'1');
-                ans.insert(0,alp);
+                ans.append(alp);
             }
         }
-        return String.valueOf(ans);
+        return String.valueOf(ans.reverse());
     }
 }
