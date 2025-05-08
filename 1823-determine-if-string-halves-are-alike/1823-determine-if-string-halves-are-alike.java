fe7b5e26d2908e4243b0673ce439ed7673efa69c
Varun Gobinath <137699472+varungobinath@users.noreply.github.com>
2025-05-08 19:28:34 +0530

    Time: 3 ms (89.24%), Space: 41.6 MB (94.36%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -4,9 +4,26 @@ class Solution {
         int n = s.length()/2;
         int count = 0;
         for(int i=0;i<n;i++){
-            if(vowels.indexOf(s.charAt(i))!=-1) count++;
-            if(vowels.indexOf(s.charAt((n)+i))!=-1) count--;
+            if(isvowels(s.charAt(i))) count++;
+            if(isvowels(s.charAt((n)+i))) count--;
         }
         return count==0;
+    }
+    public boolean isvowels(char ch){
+        switch(ch){
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                return true;
+            default:
+                return false;
+        }
     }
 }
