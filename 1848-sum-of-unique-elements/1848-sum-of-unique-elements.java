27cfb7e40301b80f37cd74a6ff3eb1690cf7a963
Varun Gobinath <137699472+varungobinath@users.noreply.github.com>
2025-05-09 14:14:01 +0530

    Time: 0 ms (100%), Space: 41.6 MB (24.19%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -1,8 +1,8 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
         int[] hash = new int[101];
-        for(int i=0;i<nums.length;i++){
-            hash[nums[i]]++;
+        for(int i : nums){
+            hash[i]++;
         }
         int sum = 0;
         for(int i=0;i<101;i++){
